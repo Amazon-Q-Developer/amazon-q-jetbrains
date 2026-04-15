e6e78d6bc2f535f4c9d979a0bc28e25ddcf423ef
Jacob Chung <chungjac@amazon.com>
2026-04-14 19:34:18 -0700

    ci-test-8: remove workspace index mocks from AmazonQLanguageClientImplTest

--- plugins/amazonq/shared/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClientImplTest.kt
+++ plugins/amazonq/shared/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClientImplTest.kt
@@ -39,10 +39,7 @@ import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.credential
 import software.aws.toolkits.jetbrains.services.codewhisperer.customization.CodeWhispererCustomization
 import software.aws.toolkits.jetbrains.services.codewhisperer.customization.CodeWhispererModelConfigurator
 import software.aws.toolkits.jetbrains.settings.CodeWhispererSettings
-import software.aws.toolkits.jetbrains.settings.CodeWhispererSettings.Companion.CONTEXT_INDEX_SIZE
-import software.aws.toolkits.jetbrains.settings.CodeWhispererSettings.Companion.CONTEXT_INDEX_THREADS
 import kotlin.random.Random
-import kotlin.random.nextInt
 
 @ExtendWith(ApplicationExtension::class)
 class AmazonQLanguageClientImplTest {
@@ -444,16 +441,9 @@ class AmazonQLanguageClientImplTest {
 
         val telemetryEnabled = Random.nextBoolean()
         val customizationArn = aString()
-        val workerThreads = Random.nextInt(CONTEXT_INDEX_THREADS)
-        val indexSize = Random.nextInt(CONTEXT_INDEX_SIZE)
-        val enableIndexing = Random.nextBoolean()
-        val enableGpu = Random.nextBoolean()
 
         val mockQSettings = mockk<CodeWhispererSettings> {
-            every { getProjectContextIndexThreadCount() } returns workerThreads
-            every { getProjectContextIndexMaxSize() } returns indexSize
-            every { isProjectContextEnabled() } returns enableIndexing
-            every { isProjectContextGpu() } returns enableGpu
+            every { isProjectContextEnabled() } returns false
         }
         mockkObject(CodeWhispererSettings.Companion)
         every { CodeWhispererSettings.getInstance() } returns mockQSettings
@@ -477,14 +467,6 @@ class AmazonQLanguageClientImplTest {
                 AmazonQLspConfiguration(
                     optOutTelemetry = !telemetryEnabled,
                     customization = customizationArn,
-                    projectContext = ProjectContextConfiguration(
-                        enableLocalIndexing = enableIndexing,
-                        enableGpuAcceleration = enableGpu,
-                        indexWorkerThreads = workerThreads,
-                        localIndexing = LocalIndexingConfiguration(
-                            maxIndexSizeMB = indexSize,
-                        )
-                    )
                 )
             )
     }
