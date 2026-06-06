c6bb383ed1032731afacfc336d3d53e3d27f0518
laileni <laileni@amazon.com>
2026-06-06 02:28:07 +0000

    deps: deprecate IDE 2025.1
    Show the deprecation notice to users on the 2025.1 (251) platform ahead
    of dropping support, now that 2026.2 is being added. Bumps MIN_VERSION
    251 -> 252 and adds a deprecation changelog entry.

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notification/MinimumVersionChange.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notification/MinimumVersionChange.kt
@@ -55,8 +55,8 @@ class MinimumVersionChange @JvmOverloads constructor(isUnderTest: Boolean = fals
     }
 
     companion object {
-        const val MIN_VERSION = 251
-        const val MIN_VERSION_HUMAN = "2025.1"
+        const val MIN_VERSION = 252
+        const val MIN_VERSION_HUMAN = "2025.2"
 
         // Used by tests to make sure the prompt never shows up
         const val SKIP_PROMPT = "aws.suppress_deprecation_prompt"
